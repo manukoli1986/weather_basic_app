bcd90a071f4744b0379f009e9ed9a9f2c646ecd1
Mayank <manukoli1986@gmail.com>
2020-07-01 03:55:16 +0530

    updated 404 and ty exception

--- app.py
+++ app.py
@@ -7,26 +7,28 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/', methods=['GET', 'POST'])
-
 def index():
-    error = None
-    if request.method == 'POST':
-        text = request.form['city']
-        city_name = text.upper()
-        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=<Enter API ID>'
-        response = requests.get(url.format(city_name)).json()
-        temperature_in_degree = "{:10.2f}".format(((response['main']['temp']) - 273.15))
-        weather_of_city = {
-            'city' : city_name,
-            'temperature' : temperature_in_degree,
-            'description' : response['weather'][0]['description'],
-            'icon' : response['weather'][0]['icon'],
-        }
-        print(weather_of_city)
-        return render_template('index.html', weather = weather_of_city, error=error)
-    
+    try:
+        error = None
+        if request.method == 'POST':
+            text = request.form['city']
+            city_name = text.upper()
+            url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=8f5ca370c7041d066e0026e9af47526f'
+            response = requests.get(url.format(city_name)).json()
+            temperature_in_degree = "{:10.2f}".format(((response['main']['temp']) - 273.15))
+            weather_of_city = {
+                'city' : city_name,
+                'temperature' : temperature_in_degree,
+                'description' : response['weather'][0]['description'],
+                'icon' : response['weather'][0]['icon'],
+            }
+            print(weather_of_city)
+            return render_template('index.html', weather = weather_of_city, error=error)
+    except:
+            return render_template('404.html')
+        
     else: 
-        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=<Enter API ID>'
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=8f5ca370c7041d066e0026e9af47526f'
         city_name = 'Lucknow'
         response = requests.get(url.format(city_name)).json()
         temperature_in_degree = "{:10.2f}".format(((response['main']['temp']) - 273.15))
@@ -38,6 +40,7 @@ def index():
         }
         print(weather_of_city)
         return render_template('index.html', weather = weather_of_city, error=error)
+
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
--- weather_script.py
+++ weather_script.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=8f5ca370c7041d066e0026e9af47526f'
 
-city_name = 'Lucknow'
+city_name = 'Del123'
 
 res = requests.get(url.format(city_name)).json()
 print(res)
