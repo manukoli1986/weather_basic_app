c458e51259eb19cfaed46f0bb524ac0fd5a35736
Mayank <manukoli1986@gmail.com>
2020-07-01 14:58:43 +0530

    removed api token

--- weather_script.py
+++ weather_script.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=8f5ca370c7041d066e0026e9af47526f'
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=XXXXX'
 
 city_name = 'Del123'
 
