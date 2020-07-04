df4f92ce5e2842144a0b4a1b389751501de2e45a
Mayank <manukoli1986@gmail.com>
2020-07-05 02:34:11 +0530

    WOrking code

--- app.py
+++ app.py
@@ -8,13 +8,12 @@ app.config['DEBUG'] = True
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    try:
-        error = None
-        if request.method == 'POST':
-            text = request.form['city']
-            city_name = text.upper()
-            url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=8f5ca370c7041d066e0026e9af47526f'
-            response = requests.get(url.format(city_name)).json()
+    if request.method == 'POST':
+        text = request.form['city']
+        city_name = text.upper()
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=8f5ca370c7041d066e0026e9af47526f'
+        response = requests.get(url.format(city_name)).json()
+        if response['cod'] == 200:
             temperature_in_degree = "{:10.2f}".format(((response['main']['temp']) - 273.15))
             weather_of_city = {
                 'city' : city_name,
@@ -23,9 +22,14 @@ def index():
                 'icon' : response['weather'][0]['icon'],
             }
             print(weather_of_city)
-            return render_template('index.html', weather = weather_of_city, error=error)
-    except:
-            return render_template('404.html')
+            return render_template('index.html', weather = weather_of_city)
+        else:
+            weather_of_city_error = {
+                'city' : city_name,
+                'country' : response['message']
+            }
+            print(weather_of_city_error)
+            return render_template('index.html', weather_error = weather_of_city_error)
         
     else: 
         url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=8f5ca370c7041d066e0026e9af47526f'
@@ -39,8 +43,8 @@ def index():
             'icon' : response['weather'][0]['icon'],
         }
         print(weather_of_city)
-        return render_template('index.html', weather = weather_of_city, error=error)
+        return render_template('index.html', weather = weather_of_city)
 
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
--- weather_script.py
+++ weather_script.py
@@ -1,8 +1,24 @@
 import requests
+from flask import request
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=XXXXX'
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=8f5ca370c7041d066e0026e9af47526f'
 
-city_name = 'Del123'
+city_name = 'London'
 
-res = requests.get(url.format(city_name)).json()
-print(res)
+response = requests.get(url.format(city_name)).json()
+if response['cod'] == 200:
+    temperature_in_degree = "{:10.2f}".format(((response['main']['temp']) - 273.15))
+    weather_of_city = {
+        'city' : city_name,
+        'temperature' : temperature_in_degree,
+        'description' : response['weather'][0]['description'],
+        'icon' : response['weather'][0]['icon'],
+    }
+    print(weather_of_city)
+else:
+    weather_of_city = {
+        'city' : city_name,
+        'country' : response['message']
+    }
+    print(weather_of_city)
+# print(response)
